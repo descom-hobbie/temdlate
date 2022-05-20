3bd79f65fda46cfda5f1891fc21cc70758b30d40
Tortitas <victorgf2011@gmail.com>
2022-05-20 11:35:50 +0200

    improvements

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 pip3 install cython &&
 cython main.py --embed &&
 PYTHONLIBVER=python$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')$(python3-config --abiflags) &&
-gcc -Os $(python3-config --includes) main.c -o temdlate $(python3-config --ldflags) -l$PYTHONLIBVER
+gcc -Os $(python3-config --includes) main.c -o temdlate $(python3-config --ldflags) -l$PYTHONLIBVER &&
+rm -rf main.c
